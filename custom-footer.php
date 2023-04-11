9ef91096673688faec2f5d4d405c3313a82895e8
Clara Swanson <claraswanson671@gmail.com>
2023-04-11 10:25:48 +0200

    Initial commit

--- custom-footer.php
+++
@@ -1,8 +0,0 @@
-<?php // Copyright (c) 2014, SWITCH ?>
-
-	<footer id="footer">
-		<p style="margin: 0px;"><?php echo $myFederationCredits; ?><a href="Geo-SWITCHwayf/about.html" target="_blank"><?php echo getLocalString('about_federation'); ?></a></p>
-	</footer>
-
-</body>
-</html>
--- default-footer.php
+++
@@ -1,9 +0,0 @@
-<?php // Copyright (c) 2014, SWITCH ?>
-	
-	<!-- Body: End -->
-		</div>
-	</div>
-
-</div>
-</body>
-</html>
